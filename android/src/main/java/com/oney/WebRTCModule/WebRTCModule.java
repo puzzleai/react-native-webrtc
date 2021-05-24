61eaedff6326459bf99ee8f3f8d8c40df5036705
Jason Kang <jasonkang14@gmail.com>
2021-05-24 12:57:26 +0900

    Fix echo for androids

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -127,7 +127,8 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
         }
 
         if (adm == null) {
-            adm = JavaAudioDeviceModule.builder(reactContext).createAudioDeviceModule();
+            adm = JavaAudioDeviceModule.builder(reactContext).setUseHardwareAcousticEchoCanceler(false)
+            .setUseHardwareNoiseSuppressor(false).createAudioDeviceModule();
         }
 
         mFactory
